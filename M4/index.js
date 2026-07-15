da02ac457563ebcf2ce240e054120bd618ced8bc
nandaarlysf <nandaarlys36@gmail.com>
2026-07-15 10:55:02 +0700

    commit kedua

--- M4/index.js
+++ M4/index.js
@@ -17,4 +17,16 @@ const pool = new Pool({
     password: 'Nanda123', //sesuaikan dengan password postgres kalian
     port: 5432,
 })
+app.get('/', (req, res) => {
+    console.log('TEST DATA :');
+    pool.query('SELECT * FROM biodata')
+        .then(testData => {
+            console.log(testData.rows);
+            res.json(testData.rows);
+        })
+        .catch(err => {
+            console.error("Error executing query", err.stack);
+            res.status(500).send("Database Error");
+        });
+});        
 
